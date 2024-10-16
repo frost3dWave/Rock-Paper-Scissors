a11765de7aa5c0ddf9019d3e63a9b6f1026e751d
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2024-10-16 15:42:04 +0530

    added logic for a round of play.

--- main.js
+++ main.js
@@ -16,4 +16,21 @@ function getUserChoice() {
     }
 }
 
+let userScore = 0;
+let computerScore = 0;
+
+function playRound(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        alert("It's a tie!");
+    }else if((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper")) {
+        alert("User Wins the round!");
+        userScore++;
+    }else{
+        alert("Computer wins the round!");
+        computerScore++;
+    }
+    // console.log(userScore);
+    // console.log(computerScore);
+}
+
 
