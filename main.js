0c1cf8297cab5e81fed099cff1e5d476a3a02ac0
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2024-10-17 21:11:52 +0530

    rewrote the file to play the game using new GUI

--- main.js
+++ main.js
@@ -1,58 +1,73 @@
+const buttons = document.querySelectorAll("button");
+
+const userScoreCard = document.querySelector(".userScoreCard");
+const computerScoreCard = document.querySelector(".computerScoreCard");
+const results = document.querySelector(".results");
+const resultsBoard = document.querySelector(".results_board");
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 
-function getUserChoice() {
-    let userChoice = prompt("Enter your choice: Rock or Paper or Scissors").toLowerCase();
-
-    if ((userChoice === "rock") || (userChoice === "paper") || (userChoice === "scissors")) {
-        return userChoice;
-    }else if(userChoice === ""){
-        getUserChoice();
-    }else{
-        getUserChoice();
-    }
-}
+buttons.forEach(button => {
+    button.addEventListener("click", getUserChoice);
+});
 
 let userScore = 0;
 let computerScore = 0;
 
-function playRound(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        alert("It's a tie!");
-    }else if((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper")) {
-        alert("User Wins the round!");
+function getUserChoice(e) {
+   let userSelection = e.target.textContent;
+   let computerSelection = getComputerChoice();
+   playRound(userSelection, computerSelection);
+}
+
+function playRound(userSelection, computerSelection) {
+    // console.log(`user:${userSelection}`);
+    // console.log(`computer:${computerSelection}`);
+    if (userSelection === computerSelection) {
+        resultsBoard.textContent = `It's a tie!`;
+    }else if((userSelection === "rock" && computerSelection === "scissors") || (userSelection === "paper" && computerSelection === "rock") || (userSelection === "scissors" && computerSelection === "paper")) {
         userScore++;
+        userScoreCard.textContent = userScore;
+        resultsBoard.textContent = `User Wins the round!\n${userSelection} beats ${computerSelection}!`;
     }else{
-        alert("Computer wins the round!");
         computerScore++;
+        computerScoreCard.textContent = computerScore;
+        resultsBoard.textContent = `Computer wins the round!\n${computerSelection} beats ${userSelection}!`;
     }
     // console.log(userScore);
     // console.log(computerScore);
+    checkWinner(userScore, computerScore);
 }
 
-function playGame() {
-    for (let i=0; i<5; i++){
-        const userSelection = getUserChoice();
-        const computerSelection = getComputerChoice();
-        playRound(userSelection, computerSelection);
-    }
-
-    console.log(`USER SCORE: ${userScore}`);
-    console.log(`COMPUTER SCORE: ${computerScore}`);
-
-    if(userScore === computerScore) {
-        alert("The game tied!");
-        console.log("The game tied!")
-    }else if(userScore > computerScore){
-        alert("User wins the Game!");
-        console.log("User wins the Game!");
-    }else{
-        alert("Computer wins the Game!");
-        console.log("Computer wins the Game!");
+function checkWinner(userScore, computerScore){
+    if(userScore === 5){
+        buttons.forEach(button => {
+            button.removeEventListener("click", getUserChoice);
+            button.setAttribute("class", "button_disabled");
+        });
+        resultsBoard.textContent = `USER WINS THE GAME!`
+        playAgain();
+    }else if(computerScore === 5){
+        buttons.forEach(button => {
+            button.removeEventListener("click", getUserChoice);
+            button.setAttribute("class", "button_disabled");
+        });
+        resultsBoard.textContent = `COMPUTER WINS THE GAME!`
+        playAgain();
     }
 }
 
-playGame();
+function playAgain(){
+    const playAgain = document.createElement("div");
+    playAgain.textContent = `PLAY AGAIN!`;
+    playAgain.setAttribute("class", "reload");
+    results.appendChild(playAgain);
+    playAgain.addEventListener("click", () => {
+        playAgain.classList.add("active");
+        window.location.reload();
+    });
+}
